b38ab83830024893eb4b60d7fdee82f2befddd3e
gabrielonet <gabriel@linux-romania.com>
2019-03-25 07:28:17 +0200

    Update protection.ino

--- protection.ino
+++ protection.ino
@@ -8,68 +8,101 @@ TFT_HX8357_Due tft = TFT_HX8357_Due(); // Invoke custom library
 #define SCALECOLOR 0xFFFF
 //Analog Measurement Declarations
 const int analogIn1 = A0;
-const int analogIn2 = A8;
-
+const int analogIn2 = A7;
+float vswr ;
 int ref_value = 0;
 int ref_LastPercent = 0;
-
+int fwd_value = 0;
+int fwd_LastPercent = 0;
+char vswr_printout[4];
+float old_vswr ;
+unsigned long time1;
+unsigned long time2;
 
 void setup() {
+Serial.begin(9600) ; 
+time1 = millis();
 tft.begin();
 tft.setRotation(1);
-Serial.begin(9600);
 tft.fillScreen(BACKCOLOR);
 tft.drawRect(2, 2, 476, 316, TFT_WHITE); // Draw bezel line
 tft.setTextColor(TFT_WHITE);  // Text colour
 tft.drawCentreString("YO8RXP 300W HF 1-30 Mhz AMPLIFIER", 240,10, 4);
 tft.drawFastHLine(2, 35, 476, TFT_WHITE);
+tft.drawRect(20, 200, 440, 50, TFT_WHITE);
+tft.setTextColor(TFT_RED);
+tft.drawCentreString("TRANSMITING !", 150,215, 4);
+tft.setTextColor(TFT_WHITE);
+
+
 tft.drawFastHLine(2, 283, 476, TFT_WHITE);
 tft.drawCentreString("| PTT | ALARM | ONLINE | 3.5 MHZ |", 240,290, 4);
-
-
-//tft.fillRect(100, 10, 30, 280, BARCOLOR);
- // primele 2 sunt originea x si y (pornesc din stanga sus), next value este latimea lui x
- // al patrulea este latimea lui y adica o latime y de 300 incepe de sus s se termina jos
-
 drawScale(); 
 }
 
 void loop(){ 
+  
+int fwd_newPercent;
 int ref_newPercent;
-ref_value = analogRead(analogIn1);
-ref_newPercent = int((ref_value/1024.0)* 100.0);
+float vswr = (float(fwd_value+ref_value)/(fwd_value-ref_value)) ;
+fwd_value = analogRead(analogIn1);
+fwd_newPercent = int((fwd_value/1024.0)* 100.0)*4;
+ref_value = analogRead(analogIn2);
+ref_newPercent = int((ref_value/1024.0)* 100.0)*4;
 
+
+if (fwd_newPercent != fwd_LastPercent){
+drawBar_FWD(fwd_newPercent); 
+  }
 if (ref_newPercent != ref_LastPercent){
-drawBar(ref_newPercent); 
-Serial.println(ref_newPercent);
-Serial.println(ref_LastPercent);
+drawBar_REF(ref_newPercent); 
+  }
 
-}
+time2 = millis();
+
+if ( (time2 -time1) >= 200  ){
+      screen(vswr);
+      time1 = millis();
+    }
 
 }
 
 void drawScale(){ 
-tft.drawCentreString("FWD", 30,75, 2);  
-tft.drawCentreString("REF", 30,110, 2);
-tft.drawFastHLine(70, 80,  400, TFT_GREEN ); // FWD horizontal line
-tft.drawFastHLine(70, 120, 400, TFT_GREEN ); // REF horizontal line 
-
+tft.drawCentreString("FWD", 30,85, 2);  
+tft.drawCentreString("REF", 30,125, 2);
+tft.fillRect(70,80,400,30,TFT_GREY);
+tft.fillRect(70,120,400,30,TFT_GREY);
 }
 
 
-void drawBar (int ref_Per){
-// primele 2 sunt originea x si y (pornesc din stanga sus), next value este latimea lui x
- // al patrulea este latimea lui y adica o latime y de 300 incepe de sus s se termina jos
- 
-if(ref_Per < ref_LastPercent){
-tft.fillRect(61, 20 + (100-ref_LastPercent), 30, ref_LastPercent - ref_Per, BACKCOLOR); 
-delay(60);
+void drawBar_FWD (int fwd_Per){
+if(fwd_Per < fwd_LastPercent){
+tft.fillRect(70 + fwd_Per, 80 , (fwd_LastPercent-fwd_Per) , 30, TFT_GREY); 
 }
 else{
-tft.fillRect(61, 20 + (100-ref_Per), 30, ref_Per - ref_LastPercent, BARCOLOR);
+tft.fillRect(70 +fwd_LastPercent,80 , (fwd_Per - fwd_LastPercent),30 , TFT_GREEN);
+} 
+fwd_LastPercent = fwd_Per; 
+}
+
+void drawBar_REF (int ref_Per){
+if(ref_Per < ref_LastPercent){
+tft.fillRect(70 + ref_Per, 120 , (ref_LastPercent-ref_Per) , 30, TFT_GREY); 
+}
+else{
+tft.fillRect(70 +ref_LastPercent,120 , (ref_Per - ref_LastPercent),30 , TFT_GREEN);
 } 
 ref_LastPercent = ref_Per; 
-
 }
+
+void screen (float test){
+String sensorVal = String(test);
+sensorVal.toCharArray(vswr_printout, 4);  
+tft.fillRect(382,201,50,39,TFT_BLACK);
+tft.drawCentreString("SWR =",340,215,4);
+tft.drawCentreString(vswr_printout,400,215,4);
+}
+
+
 
 
